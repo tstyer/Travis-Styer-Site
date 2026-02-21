b483ff82dac8d36f9238baff830d679882e1ccfb
tstyer <travisstyer.ts@gmail.com>
2026-02-21 19:00:27 +0000

    install boto and amend settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
+    # For Images
+    "storages",
 ]
 
 # -------------------------------------------------------------------
