b1bdccaca38a1c82c6fc44191e27a1bfd5689ab3
tstyer <travisstyer.ts@gmail.com>
2026-03-17 15:15:41 +0000

    Fix: js tag functionality

--- main/static/javascript/my_work.js
+++ main/static/javascript/my_work.js
@@ -1,8 +1,27 @@
-const button = document.querySelector('.tag-btn')
+// Select all the tags so I can loop through them
+const buttons = document.querySelectorAll('.tag-btn');
 
-/* Add's the active class to buttons when clicked */
 
-document.addEventListener("click", () => {
-    button.classList.add('active');
-});
+// Add's the active class to buttons when clicked
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        //then re-add to only selected:
+        button.classList.toggle("active");
 
+        // collect all the data-sets (tags) of the selected tags, ready to match with cards
+        const selectedTags = [];
+
+        buttons.forEach((button) => {
+           if (button.classList.contains("active")) {
+            selectedTags.push(button.dataset.tag);
+           }
+        });
+    });
+
+ 
+
+/* Learning note:
+- when adding classList.add... you only add it to one element. 
+- when doing classList.toggle... you change from on or off, like a switch. 
+- classList.toggle will add 'active' if not present, or remove it if it already is.
+*/
