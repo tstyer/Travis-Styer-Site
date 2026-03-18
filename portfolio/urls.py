561c3f5e10e0b4410e7e9b7bea8b3b2a23e517ff
tstyer <travisstyer.ts@gmail.com>
2026-03-18 19:45:40 +0000

    Update urls.py to amend broken image

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,9 +22,12 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
+    path("", include("main.urls")),
+]
