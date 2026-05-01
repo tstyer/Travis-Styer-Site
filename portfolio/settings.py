413730513d9719dfb96b738fbc2084f43bf92192
tstyer <travisstyer.ts@gmail.com>
2026-05-01 11:13:12 +0100

    Amend email settings host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -210,7 +210,7 @@ LOGGING = {
 # Email configuration
 # -------------------------------------------------------------------
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.livemail.co.uk"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
